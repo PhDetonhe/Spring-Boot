57413e91c755b71b41767ca2bbcabdc6eb8c6d0d
PhDetonhe <pedroh.detonhe@gmail.com>
2026-04-02 09:10:39 -0300

    Add files via upload

--- petshop/Frontend/js/fetchCategorias.js
+++ petshop/Frontend/js/fetchCategorias.js
@@ -8,7 +8,7 @@ fetch("http://localhost:8080/categorias")
           <div class="bg-slate-800 p-5 rounded-xl w-72 h-72 mx-5 my-6 border border-slate-700 
             flex flex-col justify-between
             transition-all duration-300 ease-out 
-            hover:scale-[1.04] hover:-translate-y-1 hover:shadow-2xl hover:border-blue-500/50">
+            hover:scale-110 hover:-translate-y-1 hover:shadow-2xl hover:border-blue-500/50">
 
     <!-- conteúdo -->
     <div>
@@ -43,6 +43,55 @@ fetch("http://localhost:8080/categorias")
 </div>
             `;
             tabela.innerHTML += linha;
+        });
+    })
+    .catch(error => console.error("Erro:", error));
+
+
+
+
+
+
+
+
+    fetch("http://localhost:8080/categorias")
+    .then(response => response.json())
+    .then(data => {
+        const nav = document.getElementById("categoriasnav");
+
+        nav.innerHTML = ""; // limpa antes
+
+        data.forEach(categoria => {
+            const item = `
+                <button 
+                   class="text-blue-900 text-2xl my-2 font-bold transition-all duration-300 ease-out hover:text-white hover:scale-105 hover:-translate-y-0.5"
+                    onclick="filtrarPorCategoria('${categoria.id}')"
+                >
+                    ${categoria.nome}
+                </button>
+            `;
+
+            nav.innerHTML += item;
+        });
+    })
+    .catch(error => console.error("Erro:", error));
+
+
+    fetch("http://localhost:8080/categorias")
+    .then(response => response.json())
+    .then(data => {
+        const nav = document.getElementById("catfooter");
+
+        nav.innerHTML = ""; 
+
+        data.forEach(categoria => {
+            const item = `
+                <li class="mb-4">
+                    <a href="" class="hover:underline">${categoria.nome}</a>
+                </li>
+            `;
+
+            nav.innerHTML += item;
         });
     })
     .catch(error => console.error("Erro:", error));
